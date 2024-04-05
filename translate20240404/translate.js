3c516373d16ca90e8f50559ae888a097ba6ed4e3
stephtran97 <tranducthinh.97@gmail.com>
2024-04-06 01:07:15 +0700

    init solution

--- translate20240404/translate.js
+++ translate20240404/translate.js
@@ -1,4 +1,4 @@
-let a = 'Hoc JS don gian';
+let a = 'hoc JS don gian';
 const obj = {
   hoc: 'learn',
   JS: 'JavaScript',
@@ -7,6 +7,12 @@ const obj = {
 // 'Hoc JS don gian' => 'Learn JavaScript simple'
 // 5 cach
 const translate = (str)=>{
-  
+  let result = str
+  for (let key in obj){
+    if (a.includes(key)){
+       result= result.replaceAll(key,obj[key])
+    }
+  }
+  return result;
 }
 console.log(translate(a))
