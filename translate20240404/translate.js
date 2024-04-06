8f226a52f9e9ed872899e8f27eea1c0cd2605ffa
stephtran97 <tranducthinh.97@gmail.com>
2024-04-06 22:57:29 +0700

    update

--- translate20240404/translate.js
+++ translate20240404/translate.js
@@ -1,18 +1,18 @@
-let a = 'hoc JS don gian';
+let a = "hoc JS don gian";
 const obj = {
-  hoc: 'learn',
-  JS: 'JavaScript',
-  'don gian': 'simple',
+  hoc: "learn",
+  JS: "JavaScript",
+  "don gian": "simple",
 };
 // 'Hoc JS don gian' => 'Learn JavaScript simple'
 // 5 cach
-const translate = (str)=>{
-  let result = str
-  for (let key in obj){
-    if (a.includes(key)){
-       result= result.replaceAll(key,obj[key])
+const translate = (str, dictionary) => {
+  let result = str;
+  for (let key in dictionary) {
+    if (str.includes(key)) {
+      result = result.replaceAll(key, dictionary[key]);
     }
   }
   return result;
-}
-console.log(translate(a))
+};
+console.log(translate(a, obj));
