52af47fac11cc2b8a1cc9588fe0a1890c239d994
stephtran97 <tranducthinh.97@gmail.com>
2024-04-25 00:55:07 +0700

    Test

--- test20240423/test.js
+++ test20240423/test.js
@@ -86,17 +86,50 @@ const DATA = [
   },
 ];
 
-// ********************************************************
+// --------------------------------------
 // Helper function
 // Tim user
 const findUser = (name) => {
   return DATA.find((item) => item.name === name);
 };
-// Filter nguoi khac gioi tinh
-const filterByOppositeGender = (gender) => {
-  return DATA.filter((item) => item.gender !== gender).map(getName);
-};
+
 const getName = (person) => person.name;
+const getFriends = (person) => person.friends;
+
+const filterByGender = (gender) => {
+  return DATA.filter((p) => p.gender === gender);
+};
+
+const getFriendsOfFriends = (name, gender = "all") => {
+  const person = findUser(name); // person object
+
+  if (!person) throw new Error("No person found");
+  const { friends } = person; // array friends ['a','b',...]
+
+  if (friends.length === 0) return [];
+
+  let friendsObj;
+
+  friendsObj = friends.map((fname) => findUser(fname)); // array objects [{},{},...]
+  if (friendsObj.length === 0) return [];
+
+  const friendsOfFriendsObj = friendsObj.map(getFriends).flat().map(findUser); // array objects [{},{},...]
+
+  // Filter found names by gender
+  let names;
+
+  if (gender === "all") {
+    names = friendsOfFriendsObj.map(getName).flat(); // all names
+  } else {
+    const filteredFOfFObj = friendsOfFriendsObj.filter(
+      (item) => item.gender === gender
+    );
+    names = filteredFOfFObj.map(getName).flat();
+  }
+
+  return Array.from(new Set(names)).filter((i) => i !== name);
+};
+
 // 1. Danh sach ban chung giua Tuan vs Trung
 
 const mutualFriends = (name1, name2) => {
@@ -104,16 +137,58 @@ const mutualFriends = (name1, name2) => {
   const p2 = findUser(name2);
   return p1.friends.filter((item) => p2.friends.includes(item));
 };
-console.log(mutualFriends("trung", "sy"));
+console.log(`Loi giai bai 1: ${mutualFriends("trung", "sy")}`);
 
 // 2. Suggest +4 friends cho A Khoi; ko du 4 nguoi thi suggest ntn
-// Neu user ko co friends thi suggest ban khac gioi -> ban cung gioi
-const suggestFriends = (person) => {
-  const p = findUser(person);
-  const { friends, gender } = p;
-  if (friends.length === 0) return filterByOppositeGender(gender);
+
+const getFriendsSuggestions = (name, numberOfSuggestions) => {
+  const friendsOfFriends = getFriendsOfFriends(name);
+
+  const sortedNames = DATA.map(getName).sort();
+
+  if (friendsOfFriends.length === 0) {
+    return sortedNames.slice(0, numberOfSuggestions); // TODO: suggest ppl sorted alphabetically
+  }
+
+  if (friendsOfFriends.length < numberOfSuggestions) {
+    const addingSortedNames = sortedNames.slice(
+      0,
+      numberOfSuggestions - friendsOfFriends.length
+    );
+    return [...friendsOfFriends, ...addingSortedNames]; // TODO: suggest more till length = numberOfSuggestions
+  }
+  if (friendsOfFriends.length >= numberOfSuggestions)
+    return friendsOfFriends.slice(0, 4); // TODO: if result is sufficient of the number of suggestions
 };
+console.log(`Loi giai bai 2: ${getFriendsSuggestions("khoi", 4)}`);
 
 // 3. Tim ban gai cho Trung
 
-// 4. Tim ban cho Hoa: chi ket ban vs nu, sap xep theo do than thiet -> thu tu bang chu cai
+const findFriend = (name, gender) => {
+  const friendsOfFriends = getFriendsOfFriends(name, gender);
+  // Suggest friend theo do than thiet, bang chu cai
+  return Array.from(
+    new Set(
+      [
+        ...friendsOfFriends,
+        ...filterByGender(gender).map(getName).sort(),
+      ].filter((n) => n !== name)
+    )
+  );
+};
+console.log(`Loi giai bai 3: ${findFriend("trung", EGender.female)}`);
+
+// 4. Suggest ban nu cho Hoa, sap xep theo than thiet , bang chu cai
+const findNewFriend = (name, gender) => {
+  const person = findUser(name);
+  const friendsOfFriends = getFriendsOfFriends(name, gender);
+  // Suggest new friend theo do than thiet, bang chu cai
+  return Array.from(
+    new Set(
+      [...friendsOfFriends, ...filterByGender(gender).map(getName).sort()]
+        .filter((n) => !person.friends.includes(n))
+        .filter((n) => n !== name)
+    )
+  );
+};
+console.log(`Loi giai bai 4: ${findNewFriend("hoa", EGender.female)}`);
